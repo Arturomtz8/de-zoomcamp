b8181885918df81a49d177eb9022562400b756b4
arturo.mtz8@gmail.com <arturo.mtz8@gmail.com>
2023-02-11 20:49:00 +0000

    working dbt connection

---  week-04/models/staging/trip_data.sql
+++  week-04/models/staging/trip_data.sql
@@ -1,5 +1,6 @@
-{{ config(materialized="view") }}
+{{ config(materialized="table") }}
 
-select *
-from {{ source('staging','nyc_trips') }}
-limit 10
+  select *,
+    row_number() over(partition by vendorid, tpep_pickup_datetime) as rn
+  from {{ source('staging','nyc_trips') }}
+  limit 10
