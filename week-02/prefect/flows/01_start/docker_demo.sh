9e60d6610112cb4852cd6c79839bcc02709a883c
Arturomtz8 <arturo.mtz8@gmail.com>
2023-09-06 10:17:46 -0600

    added pending file modified

--- week-02/prefect/flows/01_start/docker_demo.sh
+++ week-02/prefect/flows/01_start/docker_demo.sh
@@ -6,6 +6,7 @@ prefect deployment build ./week-02/prefect/flows/01_start/homework_github_deploy
   -o prefect-docker-deployment \
   --apply
 
+
 # {
 #     "EXTRA_PIP_PACKAGES":  "pandas==1.5.2 prefect-gcp[cloud_storage]==0.2.4 protobuf==4.21.11 pyarrow==10.0.1 pandas-gbq==0.18.1"
 # }
